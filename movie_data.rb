6020409bd1aa9bb3120ab1e213154fd92f1d0f82
Samir Undavia <samir1@brandeis.edu>
2014-01-23 15:43:49 -0500

    added methods and class variables

--- movie_data.rb
+++ movie_data.rb
@@ -2,9 +2,10 @@
 
 class MovieData
 
-	def initialize (folder, set=nil)
-		@popularityMovieToRatings = Hash.new
-		@userRatings = Hash.new
+	def initialize folder, set=nil
+		@movieRatingSum = Hash.new # {movie_id => sum of ratings}
+		@userMovieRating = Hash.new # {user_id => {movie_id => rating}}
+		@movieUserRating = Hash.new # {movie_id => {user_id => rating}}
 		@folder = folder
 		@set = set
 	end
@@ -15,27 +16,34 @@ class MovieData
 		else
 			file = [@set, "base"].join(".")
 		end
-
-		File.foreach(@folder + "/#{file}") do |line|
+		File.foreach @folder + "/#{file}" do |line|
 			lineArray = line.split("\t").map(&:to_i)
-			if !@popularityMovieToRatings.has_key?(lineArray[1])
-				@popularityMovieToRatings[lineArray[1]] = 0
+            if !@movieRatingSum.has_key?(lineArray[1])
+                    @movieRatingSum[lineArray[1]] = 0
+            end
+            @movieRatingSum[lineArray[1]] += lineArray[2]
+			if !@userMovieRating.has_key?(lineArray[0])
+				@userMovieRating[lineArray[0]] = Hash.new
 			end
-			@popularityMovieToRatings[lineArray[1]] += lineArray[2]
-			if !@userRatings.has_key?(lineArray[0])
-				@userRatings[lineArray[0]] = Hash.new
+			@userMovieRating[lineArray[0]][lineArray[1]] = lineArray[2]
+			if !@movieUserRating.has_key?(lineArray[1])
+				@movieUserRating[lineArray[1]] = Hash.new
 			end
-			@userRatings[lineArray[0]][lineArray[1]] = lineArray[2]
+			@movieUserRating[lineArray[1]][lineArray[0]] = lineArray[2]
 		end
 	end
 
 	# popularity is the sum of all scores for each movie because if a movie had low scores, but a lot of people rated it, it is sill a popular movie
-	def popularity(movie_id)
-		return @popularityMovieToRatings[movie_id]
+	def popularity movie_id
+		pop = 0
+		popArray = @movieUserRating[movie_id].values
+		popArray.each { |a| pop+=a }
+		return pop
+		#return @movieRatingSum[movie_id]
 	end
 
 	def popularity_list
-		h = @popularityMovieToRatings.sort_by {|k,v| v}.reverse
+		h = @movieRatingSum.sort_by {|k,v| v}.reverse
 		list = Array.new
 		for x in h
 			list.push(x[0])
@@ -44,9 +52,9 @@ class MovieData
 	end
 
 	# similarity sees is user1 has any movies rated in common with user2. If there is a common movie rated, 5-(the distance away from the ratings) is added to similarity, so ratings that are closer to each other have a higher similarity
-	def similarity(user1,user2)
-		arr1 = @userRatings[user1]
-		arr2 = @userRatings[user2]
+	def similarity user1,user2
+		arr1 = @userMovieRating[user1]
+		arr2 = @userMovieRating[user2]
 		similar = 0
 		for x in arr1.keys
 			if arr2.has_key?(x)
@@ -56,10 +64,10 @@ class MovieData
 		return similar
 	end
 
-	def most_similar(u)
+	def most_similar u
 		sim = 0
 		user = 0
-		for x in @userRatings.keys
+		for x in @userMovieRating.keys
 			if x != u
 				currentSim = similarity(u,x)
 				if currentSim > sim
@@ -71,9 +79,29 @@ class MovieData
 		return user
 	end
 
+	#def rating u,m
+
+	def predict u,m
+		if @userMovieRating[most_similar(u)].has_key?(m)
+			return @userMovieRating[most_similar(u)][m]
+		else
+			predict(most_similar(u),m)
+		end
+	end
+
+	def movies u
+		return @userMovieRating[u].keys
+	end
+
+	def viewers m
+		return @movieUserRating[m].keys
+	end
+
+	#def run_test k
+
 end
 
-movie = MovieData.new("ml-100k",:u1)
+movie = MovieData.new("ml-100k")
 movie.load_data
 popList = movie.popularity_list
 puts "first and last ten elements of popularity list"
